4ceb03f7903f4e5c1335b434840da939d7b73681
XARIA <37904871+xaria-coin@users.noreply.github.com>
2018-08-31 18:16:39 +0100

    Update config.js

--- website_example/config.js
+++ website_example/config.js
@@ -6,8 +6,8 @@ var discord = "https://discordapp.com/invite/YourPool";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var blockchainExplorer = "http://explorer.xaria.xyz/?hash={id}#blockchain_block";
+var transactionExplorer = "http://explorer.xaria.xyz/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
